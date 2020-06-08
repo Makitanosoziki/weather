d79af45e8e9cdacbfd382635dccdfc572c6dfce4
makitanosoziki <noxon.ff@gmail.com>
2020-06-09 07:22:39 +0900

    infoの天気文つけた

--- components/InfoWeather.vue
+++ components/InfoWeather.vue
@@ -1,6 +1,7 @@
 <template>
     <section>
         <div class="info-main"><img :src="INFO_MAINIMG" alt="" class="info-main-img"></div>
+        <div class="info-mainDiscription">{{ INFO_MAINDISCRIPTION }}</div>
     </section>
 </template>
 
@@ -33,6 +34,29 @@ export default {
                 return require('@/assets/sunny.png')
             }
         },
+        INFO_MAINDISCRIPTION() {
+            const mainArray = []
+            for (let i=0; i<7; i++) {
+                const main = this.forecastItem && this.forecastItem.list[i].weather[0].main
+                mainArray.push(main)
+            }
+            if (mainArray.every(value => value === value)) {
+                const main = mainArray[0]
+                if(main === 'Clear') {
+                    return 'はれ'
+                } else if(main === 'Clouds') {
+                    return 'くもり'
+                } else if(main === 'Rain') {
+                    return 'あめ'
+                } else if(main === 'Thunderstorm') {
+                    return 'かみなり'
+                } else if(main === 'Snow') {
+                    return 'ゆき'
+                }
+            } else {
+                return 'その他'
+            }
+        },
         ...mapState({
             forecastItem: state => state.api.forecastItem
         }),
@@ -56,6 +80,15 @@ export default {
     width: 33%;
     left: 20%;
     top: 30%;
+}
+
+.info-mainDiscription {
+    position: absolute;
+    right: 18%;
+    top: 18.5%;
+    color: #3e1b28;
+    font-size: 3vw;
+    font-family: 'M PLUS 1p', sans-serif;
 }
 
 </style>
