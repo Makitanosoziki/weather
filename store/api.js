b64f98c5e3ece953f1364a19b3466d6efc59adac
makitanosoziki <noxon.ff@gmail.com>
2020-06-09 02:36:09 +0900

    forecastItemとれるようにした

--- store/api.js
+++ store/api.js
@@ -1,5 +1,6 @@
 export const state = () => ({
-    weatherItem: undefined
+    weatherItem: undefined,
+    forecastItem: undefined
 })
 
 const appid = '7e5327dc967ee87497d613e06eb772a0'
@@ -9,11 +10,18 @@ export const actions = {
     async getWeather({ commit }){
         const { data } = await this.$axios.get(`${weatherApiUrl}weather?units=metric&q=Tokyo&appid=${appid}`)
         commit('GET_WEATHER', data)
+    },
+    async getForecast({ commit }) {
+        const { data } = await this.$axios.get(`${weatherApiUrl}forecast?units=metric&q=Tokyo&cnt=7&appid=${appid}`)
+        commit('GET_FORECAST', data)
     }
 }
 
 export const mutations = {
     GET_WEATHER(state, data){
         state.weatherItem = data
+    },
+    GET_FORECAST(state, data){
+        state.forecastItem = data
     }
 }
